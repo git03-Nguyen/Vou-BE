264b06d63fd179cc2842817a3bc535c1f1adf016
DinhAnh <nguyendinhanhvlqt@gmail.com>
2025-01-12 15:53:32 +0700

    Update codee

--- src/ApiGateway/Program.cs
+++ src/ApiGateway/Program.cs
@@ -20,7 +20,8 @@ public class Program
         builder.Services
             .ConfigureAuthentication(builder.Configuration)
             .ConfigureOcelot(builder.Configuration)
-            .ConfigureCors(builder.Configuration);
+            .ConfigureCors(builder.Configuration)
+            .AddSignalR();
         
         var app = builder.Build();
 
